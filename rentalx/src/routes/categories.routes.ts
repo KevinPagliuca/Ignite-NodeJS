9b17147c962cd768bbef621401f524580b6c7b02
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2021-09-25 01:31:32 -0300

    SOLID principles

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,19 +1,15 @@
 import { Request, Response, Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req: Request, res: Response) => {
   const { name, description } = req.body;
-  const categorieAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categorieAlreadyExists) {
-    return res.status(400).json({ message: 'Category already exists' });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
   return res.status(201).send();
 });
 
@@ -22,4 +18,5 @@ categoriesRoutes.get('/', (req: Request, res: Response) => {
 
   res.status(200).json(all);
 });
+
 export { categoriesRoutes };
