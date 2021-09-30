5a815d4309c4fe08c3504f5b4912b8567f970ec9
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2021-09-30 00:43:35 -0300

    SOLID principles

---
+++ rentalx/src/services/CreateSpecificationService.ts
@@ -0,0 +1,16 @@
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor() {
+    console.log('TODO');
+  }
+
+  execute({ name, description }: IRequest): void {
+    console.log('TODO');
+  }
+}
+
+export { CreateSpecificationService };
