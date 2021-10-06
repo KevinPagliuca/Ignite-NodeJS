85e11c5c92942d37d2b9c2c2161570166b7a0b60
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2021-10-06 02:37:15 -0300

    json web token and session routes for user

---
+++ rentx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,59 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+    id: string;
+    driver_license: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const token = sign({}, '8e4103323c9108df5ac44091ec21959d', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+        driver_license: user.driver_license,
+      },
+    };
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- rentx/src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ rentx/src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -16,6 +16,12 @@ class CreateUserUseCase {
     email,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists!');
+    }
+
     const passwordHash = await hash(password, 8);
     await this.usersRepository.create({
       name,
