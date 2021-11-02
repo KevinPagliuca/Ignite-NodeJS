fc70bf41aaafd1fc5d59cb33f296f7a9dfa352eb
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2021-11-02 01:49:32 -0300

    feat: finishing rentals

--- rentx.backend/src/modules/rentals/useCases/createRental/CreateRentalController.ts
+++ rentx.backend/src/modules/rentals/useCases/createRental/CreateRentalController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 class CreateRentalController {
   async handle(req: Request, res: Response): Promise<Response> {
-    return res.send();
+    const { id } = req.user;
+    const { expected_return_date, car_id } = req.body;
+
+    const createRentalUseCase = container.resolve(CreateRentalUseCase);
+
+    const rental = await createRentalUseCase.execute({
+      user_id: id,
+      car_id,
+      expected_return_date,
+    });
+    return res.status(201).json(rental);
   }
 }
 
